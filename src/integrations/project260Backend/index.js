4709f2590819d0be575c3546d4e08deda87f71e4
chinyasuhail <suhail@chinyasuhail.com>
2022-12-04 04:18:21 +0530

    adds backend integration

---
+++ src/integrations/project260Backend/index.js
@@ -0,0 +1,62 @@
+const axios = require('axios').default;
+
+const baseUrl = 'https://project260.xyz/api/v1';
+
+const getProposals = async ({ safeAddress }) => {
+  const url = `${baseUrl}/proposals?safeAddress=${safeAddress}`;
+
+  const response = await axios.get(url, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    timeout: 10000,
+  });
+
+  if (response.status !== 200) {
+    throw new Error('Failed to get proposals');
+  }
+
+  return response.data;
+}
+
+const createProposal = async (data) => {
+  const response = await axios.post(`${baseUrl}/proposals`, data, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    timeout: 10000,
+    body: {
+      ...data
+    }
+  });
+
+  if (response.status !== 201) {
+    throw new Error('Failed to create proposal');
+  }
+
+  return response.data;
+}
+
+const approveProposal = async (data) => {
+  const response = await axios.post(`${baseUrl}/proposals/approve`, data, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    timeout: 10000,
+    body: {
+      ...data
+    }
+  });
+
+  if (response.status !== 200) {
+    throw new Error('Failed to approve proposal');
+  }
+
+  return response.data;
+}
+
+module.exports = {
+  getProposals,
+  createProposal,
+  approveProposal,
+}
